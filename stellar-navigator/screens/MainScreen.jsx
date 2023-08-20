18e9cb0c86b2133ffb49a5b3cf20b2a5762becff
Stuart-Yee <ki6fzb@gmail.com>
2023-08-19 21:40:28 -0700

    initial commit

---
+++ stellar-navigator/screens/MainScreen.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native";
+
+const MainScreen = (props) => {
+
+    return <View>
+        <Text>
+            My Main Screen
+        </Text>
+    </View>
+} 
+
+export default MainScreen;
---
+++ stellar-navigator/screens/SystemView.jsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native"
+
+const SystemView = (props) => {
+    return(
+        <View>
+            <Text>This is a star</Text>
+        </View>
+    )
+}
+
+export default SystemView;
