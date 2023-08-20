c014a5da2efedac5e7e064041d92ef0b3676e7ff
Stuart-Yee <ki6fzb@gmail.com>
2023-08-19 22:15:27 -0700

    adding navigation

--- stellar-navigator/screens/MainScreen.jsx
+++ stellar-navigator/screens/MainScreen.jsx
@@ -1,12 +1,28 @@
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 
 const MainScreen = (props) => {
 
-    return <View>
-        <Text>
+    return (
+    <View style={styles.screenContainer}>
+        <Text style={styles.text}>
             My Main Screen
         </Text>
     </View>
+    )
 } 
 
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text: {
+        color: "#000",
+        // borderColor: "black",
+        // borderWidth: 1
+    }
+})
+
 export default MainScreen;
+
--- stellar-navigator/screens/SystemView.jsx
+++ stellar-navigator/screens/SystemView.jsx
@@ -1,11 +1,24 @@
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet} from "react-native"
 
 const SystemView = (props) => {
     return(
-        <View>
-            <Text>This is a star</Text>
+        <View style={styles.screenContainer}>
+            <Text style={styles.text}>This is a star</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text: {
+        color: "#000",
+        // borderColor: "black",
+        // borderWidth: 1
+    }
+})
 
 export default SystemView;
